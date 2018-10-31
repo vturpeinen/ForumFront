238d02133abfc774bb93118d1d7b952f52fef41b
vturpeinen <vturpeinen@hotmail.com>
2018-10-31 15:00:28 +0200

    Frontti v1

--- .idea/js/Poisto.js
+++ .idea/js/Poisto.js
@@ -1,25 +1,18 @@
-
-function lahetaClick() {
+function poistaClick() {
     console.log("Click");
     var data = JSON.stringify({
-        "label":  $("#label").val(),
-        "content": $("#content").val(),
-        "id": 5,
-        "ttimestamp": "2018-11-22",
-        "ggroup": "Ryhmähau"
+        "id": $("#id").val(),
     });
-    lahetys(data);
+    poisto(data);
 }
-function lahetys(data) {
+function poisto(data) {
     var xhr = new XMLHttpRequest();
-  //  xhr.withCredentials = true;
-
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
             console.log(this.responseText);
         }
     });
-    xhr.open("POST", "http://localhost:8080/messages");
+    xhr.open("DELETE", "http://localhost:8080" + "/id");
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.send(data);
 }
